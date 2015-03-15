989990fd5eaa8becbb0c436f4ce7f9ec6ff8d17e
Aaron Bieber <aaron@aaronbieber.com>
2015-03-15 15:31:23 -0400

    Add logging levels and summary reports.

--- lamplighter.py
+++ lamplighter.py
@@ -34,34 +34,73 @@ import urllib
 on_home = lambda: None
 on_away = lambda: None
 
+# Definition of log levels.
+LOG_NONE  = 0
+LOG_WARN  = 1
+LOG_INFO  = 2
+LOG_DEBUG = 3
+
+# Just a couple of globals for aggregating stats.
+scans         = 0
+state_changes = 0
+
 def run():
     create_pidfile()
     config.load()
     signal.signal(signal.SIGTERM, handle_term)
     signal.signal(signal.SIGHUP, handle_hup)
+    start_time  = datetime.datetime.now().replace(microsecond = 0)
+    last_report = None
+    
+    log("Lamplighter has started.", LOG_NONE)
+    log("Logging level is set to %s." % config.config["log_level"], LOG_NONE)
+    
+    if config.config["report_frequency"] is 0 or \
+       globals()[config.config["log_level"]] < LOG_WARN:
+        log("A summary report will not be logged.", LOG_NONE)
+    else:
+        log("A summary report will be logged every %s seconds." % config.config["report_frequency"], LOG_NONE)
     
     while True:
+        last_report = maybe_print_stats(start_time, last_report)
         search()
         time.sleep(1)
         
+def maybe_print_stats(start_time, last_report):
+    global scans
+    global state_changes
+    
+    if config.config["report_frequency"] is 0:
+        return
+
+    now = int(datetime.datetime.now().strftime("%s"))
+    if last_report is None or now - last_report > config.config["report_frequency"]:
+        last_report = int(datetime.datetime.now().strftime("%s"))
+        running_for = datetime.datetime.now().replace(microsecond = 0) - start_time
+        log("Running for %s. Performed %s scan(s), changed state %s time(s)." % (running_for,
+                                                                                   scans,
+                                                                                   state_changes),
+            LOG_WARN)
+        
+    return last_report
+        
 def search():
     """The main thread."""
-    
     quiet_hours = ""
+    state = current_state()
+
     if within_quiet_hours():
         quiet_hours = " (quiet hours)"
-    log("--- Commencing search%s ---" % quiet_hours)
-
-    state = current_state()
+    log("Commencing search%s" % quiet_hours, LOG_INFO)
 
     device_count = False
     confirm_with_arp = state == "home" or state == False
     while device_count is False:
-        log("Finding initial device count...")
+        log("Finding initial device count...", LOG_DEBUG)
         device_count = count_devices_present(confirm_with_arp = confirm_with_arp)
         
     if state == False:
-        log("No current state. Initializing.")
+        log("No current state. Initializing.", LOG_DEBUG)
 
         if device_count is 0:
             log("State initialized to away.")
@@ -72,7 +111,7 @@ def search():
 
         save_state(state)
     else:
-        log("Current state is %s." % state)
+        log("Current state is %s." % state, LOG_DEBUG)
 
     # Either due to wireless network blips or general unreliability of
     # a single network scan, these scans are guaranteed to be correct
@@ -94,33 +133,34 @@ def search():
     # we'll transition to "away."
     if state == "home" and device_count is 0:
         # Delay ten seconds and then check three more times.
-        log("*** Possible change to away; wait 10 sec. and search 3 more times...")
+        log("*** Possible change to away; wait 10 sec. " + \
+            "and search 3 more times...", LOG_INFO)
         time.sleep(10)
 
         if confirm_device_count_is_zero():
-            log("Devices confirmed missing. State changed to away.")
+            log("State changed to away.", LOG_WARN)
             save_state("away")
 
             if within_quiet_hours():
-                log("Within quiet hours! Triggering on_away_quiet_hours() callback.")
+                log("Triggered on_away callback for quiet hours.", LOG_INFO)
                 on_away(True)
             else:
-                log("Triggering on_away() callback.")
+                log("Triggered on_away callback.", LOG_INFO)
                 on_away(False)
 
     elif state == "away" and device_count > 0:
-        log("*** State changing to home.")
+        log("State changed to home.", LOG_WARN)
         save_state("home")
 
         if within_quiet_hours():
-            log("Within quiet hours! Triggering on_home_quiet_hours() callback.")
+            log("Triggered on_home callback for quiet hours.", LOG_INFO)
             on_home(True)
         else:
-            log("Triggering on_home() callback.")
+            log("Triggered on_home callback.", LOG_INFO)
             on_home(False)
         
     else:
-        log("State is '%s', device count is %s; nothing to do." % (state, device_count))
+        log("State is '%s', device count is %s; nothing to do." % (state, device_count), LOG_INFO)
 
 def within_quiet_hours():
     now = datetime.datetime.now()
@@ -145,25 +185,26 @@ def within_quiet_hours():
         return True
         
 def confirm_device_count_is_zero():
-    log("*** Performing 3 confirmation searches...")
+    log("*** Performing 3 confirmation searches...", LOG_DEBUG)
 
     for x in range(3):
         test = count_devices_present(confirm_with_arp = True)
-        log("*** Found %s device(s)." % test)
+        log("*** Found %s device(s)." % test, LOG_DEBUG)
 
         if test is not 0:
-            log("*** False alarm, device(s) found.")
+            log("*** False alarm, device(s) found.", LOG_INFO)
             return False
 
         time.sleep(5)
 
     return True
 
-def log(message):
+def log(message, level = LOG_WARN):
     """Output a pretty log message."""
-    pid = os.getpid()
-    now = time.strftime("%Y-%m-%d %H:%M:%S")
-    print "[%s] %s: %s" % (pid, now, message)
+    if globals()[config.config["log_level"]] >= level:
+        pid = os.getpid()
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+        print "[%s] %s: %s" % (pid, now, message)
 
 def state_file_path():
     """Return the path to the state file."""
@@ -175,6 +216,7 @@ def save_state(state):
     if os.path.isfile(statefile_name):
         os.unlink(statefile_name)
     file(statefile_name, "w").write(state)
+    state_changes += 1
 
 def current_state():
     """Find the current (last saved) state."""
@@ -192,19 +234,19 @@ def get_pidfile_name():
     return str("/var/run/lamplighter.pid")
 
 def handle_hup(signum, frame):
-    log("Received SIGHUP, reloading config file.")
+    log("Received SIGHUP, reloading config file.", LOG_WARN)
     config.load()
 
 def handle_term(signum, frame):
     """Clean up and exit."""
-    log("Received SIGTERM; cleaning up and exiting.")
+    log("Received SIGTERM; cleaning up and exiting.", LOG_WARN)
     os.unlink(get_pidfile_name())
     sys.exit(0)
 
 def create_pidfile():
     """Create a pidfile for this process."""
     pid = str(os.getpid())
-    log("Creating pidfile for %s" % pid)
+    log("Creating pidfile for %s" % pid, LOG_DEBUG)
     file(get_pidfile_name(), "w").write(pid)
 
 def count_devices_present(confirm_with_arp = False):
@@ -223,26 +265,29 @@ def count_devices_present(confirm_with_arp = False):
     count = count_devices_present_nmap()
 
     if confirm_with_arp and count is 0:
-        log("nmap returned zero; waiting one second and confirming with arp.")
+        log("nmap returned zero; waiting one second and confirming with arp.", LOG_DEBUG)
         time.sleep(1)
         count = count_devices_present_arp()
 
     return count
     
 def count_devices_present_arp():
-    log("Searching for devices with arp-scan.")
+    global scans
+    log("Searching for devices with arp-scan.", LOG_DEBUG)
     try:
         device_search = subprocess.check_output(["sudo",
                                                  "arp-scan",
                                                  "192.168.10.0/24"])
+        scans += 1
     except subprocess.CalledProcessError:
-        log("arp-scan returned a non-zero exit status!")
+        log("arp-scan returned a non-zero exit status!", LOG_WARN)
         return False
 
     return count_devices_in_string(device_search)
     
 def count_devices_present_nmap():
-    log("Searching for devices with nmap.")
+    global scans
+    log("Searching for devices with nmap.", LOG_DEBUG)
     try:
         device_search = subprocess.check_output(["sudo",
                                                  "nmap",
@@ -250,8 +295,9 @@ def count_devices_present_nmap():
                                                  "-n",
                                                  "-T5",
                                                  "192.168.10.0/24"])
+        scans += 1
     except subprocess.CalledProcessError:
-        log("nmap returned a non-zero exit status!")
+        log("nmap returned a non-zero exit status!", LOG_WARN)
         return False
 
     return count_devices_in_string(device_search)
@@ -260,7 +306,7 @@ def count_devices_in_string(search_string):
     count = 0
     for name in config.config['devices']:
         if search_string.lower().find(config.config['devices'][name].lower()) > -1:
-            log("Found %s's device." % name.title())
+            log("Found %s's device." % name.title(), LOG_INFO)
             count += 1
 
     return count
